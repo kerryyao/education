defac0c87d34faf73a96683ad8a367a04bd68314
姚苏国 <suguo.yao@SuGuo-MacBook-Air.local>
2015-11-15 19:32:32 +0800

    ready for database

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/message',function(req, res, next){
+	var nicename=req.body.nicename;
+	var email=req.body.email;
+	var title=req.body.title;
+	var content=req.body.content;
+
+	console.log(nicename+email+title+content);
+	res.send("hello");
+});
+
 module.exports = router;
